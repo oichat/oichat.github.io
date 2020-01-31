a1cf4bbe43239825022f5f25c7ff508e706ededa
oichat <msarsilmaz45@gmail.com>
2020-01-31 22:47:28 +0600

    1.3

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -112,11 +112,10 @@ function decrypt(message, password){
     $("#chat_status").val(chatStatus);
     console.log( $("#chat_status").val());
     $("#friend_image").attr("src", friendPhotoUrl);
-     
     $(".chat-screen .body").html("");
 
 /*-----start---------------get realtime messages data-----------------------------*/ 
-    var html = ""; 
+  /*  var html = ""; 
     var q = dbRef.collection('messages').orderBy("msgtime").where("friendship_id", "==", $("#friendship_id").val());
      q.onSnapshot(function(snapshot) {
         snapshot.docChanges().forEach(function(change) {
@@ -150,7 +149,7 @@ function decrypt(message, password){
         });
          $(".chat-screen .body").html(html); 
          $(".chat-screen .body").animate({scrollTop: $(".chat-screen .body").prop("scrollHeight")}, 0); 
-    });
+    });*/
     /*-----end---------------get realtime messages data-----------------------------*/     
         
     
